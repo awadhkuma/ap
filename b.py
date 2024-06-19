d48eecfdb8e9d0f117866c792508eb4e5ce8aa5c
awadh <awadhkumarkumar421@gmail.com>
2024-06-19 14:29:42 +0000

    revision

---
+++ b.py
@@ -0,0 +1,38 @@
+# list=[1,2,3,4,5,6,7,8,9]
+# b=0
+# for a in list:
+#     b+=a
+#     print(type(list))
+#     print(b)
+
+# a=int(input("enter a score century:"))
+# if a==50:
+#     print("half century")
+# elif a==100:
+#     print("full century")
+# elif a==200:
+#     print("duble century")
+# elif a==300:
+#     print("triple century")            
+
+
+# num="hello world"
+# print(num)
+# print(type(num))
+
+# num="5.5"
+# print(num)
+# print(type(num))
+
+# for i in range(0,11):
+#     print (i)
+
+# t=int(input("enter any number:"))
+# a=1
+# while a<=10:
+#     print(t*a)
+#     a=a+1
+
+c=int(input("enter the celsius:"))
+f=(c*9/5)+32
+print(f)
