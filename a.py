8cec251fbb87f8c0455d5ef70b85c5018a26f099
awadh <awadhkumarkumar421@gmail.com>
2024-07-08 15:19:55 +0000

    Done

--- a.py
+++ a.py
@@ -1,62 +1,10 @@
-# table =int(input("enter any number:"))
-# a=1
-# while a<=10:
-#     print(table*a)
-# #     a=a+1
-# import numpy as np
-# a=np.array(([1,2,3,4,5],[1,2,3,4,5]))
-# print(a)
-# print(type(a))
-# print(a.ndim)
-
-# pi=3.14
-# redius=float(input("enter a number:"))
-# aria=pi*redius*redius
-# circumference=2*pi*redius
-# print(redius)
-# print(circumference)
-
-
-# import cmath
-# a=float(int(input("enter a:")))
-# b=float(int(input("enter b:")))
-# c=float(int(input("enter c:")))
-# d=(b**2-(4*a*c))
-# sol1=(-b-cmath.sqrt(d))/(2*a)
-# sol2=(-b+cmath.sqrt(d))/(2*a)
-# print("format",(sol
-
-# x=100
-# y=20
-# print("before swape x value is ",x,"/n y value",y)
-# x,y=y,x
-# print("after swape x value is",x, "/n y value is " ,y)
-
-# import random
-# print(random.randint(0,9))
-
-# kilometers=float(input("enter value  kilometers:"))
-# miles=kilometers*0.621371
-# print(kilometers,miles)
-
-# c=float(input("enter celsius:"))
-# far=(c*1.8)+32
-# print(c,far)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ln = []
+while True:
+    a = input("Enter a number (or No to stop): ")
+    if a == "No":
+        ln.sort()
+        print(ln)
+        print(ln[-2])
+        break
+    else:
+        ln.append(int(a))
